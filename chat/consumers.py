eed37809852f1617766ec859749f56a13513ea80
ffenland <ffenland@gmail.com>
2023-07-31 12:18:24 +0900

    socket complete

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,34 +1,35 @@
 import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    print("HI")
-
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+        self.room_group_name = f"chat_{self.room_name}"
 
-        # 채팅 그룹에 입장
+        # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
-        # 채팅 그룹에서 퇴장
+        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
+    # Receive message from WebSocket
     async def receive(self, text_data):
-        # 클라이언트로부터 메시지 수신
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # 클라이언트에게 메시지 전송
+        # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {"type": "chat.message", "message": message}
         )
 
+    # Receive message from room group
     async def chat_message(self, event):
-        # 클라이언트로 메시지 전송
         message = event["message"]
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
