5af67546e743d73946a69f5952741dd671608ed8
ffenland <ffenland@gmail.com>
2023-08-18 11:57:18 +0900

    asdfadsf

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,10 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
 from common.models import CommonPKModel, CommonModel
+import channels.layers
+from asgiref.sync import async_to_sync
 
 # Create your models here.
 
--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .consumers import ChatConsumer
+
 from .views import ChatRoomListView, ChatMessageListView
 
-websocket_urlpatterns = [
-    path("chat/<str:room_name>/", ChatConsumer.as_asgi()),
-]
 
 urlpatterns = [
     path("", ChatRoomListView.as_view()),
