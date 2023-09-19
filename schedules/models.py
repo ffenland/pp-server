b5ef97765ef5799a706d1aa75338aee51e19fe62
ffenland <ffenland@gmail.com>
2023-09-19 15:26:03 +0900

    ㅇㅇ

--- schedules/models.py
+++ schedules/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.core.validators import RegexValidator
-from common.models import CommonPKModel
+from common.models import CommonPKModel, CommonModel
 
 # Create your models here.
 
